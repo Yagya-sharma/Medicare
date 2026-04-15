820dc08f72379e9c6ea732ee37ed2ad79807b8e3
Yagya sharma <sharmayagya74@gmail.com>
2026-04-15 18:37:11 +0530

    new features

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -8,22 +8,22 @@ const Sidebar = () => {
 
     const {aToken}=useContext(AdminContext)
   return (
-    <div>
+    <div className='min-h-screen bg-white border-r'>
       {
-        aToken && <ul>
-            <NavLink to={'/admin-dashboard'}>
+        aToken && <ul className='text-[#515151] mt-5'>
+            <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5F6FFF] ':''}`} to={'/admin-dashboard'}>
                 <img src={assets.home_icon}/>
                 <p>Dashboard</p>
             </NavLink>
-            <NavLink to={'/all-appointments'}>
+            <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5F6FFF] ':''}`} to={'/all-appointments'}>
                 <img src={assets.appointment_icon}/>
                 <p>Appointments</p>
             </NavLink>
-            <NavLink to={'/add-doctor'}>
+            <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5F6FFF] ':''}`} to={'/add-doctor'}>
                 <img src={assets.add_icon}/>
                 <p>Add Doctor</p>
             </NavLink>
-            <NavLink to='/doctor-list'>
+            <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5F6FFF] ':''}`} to='/doctor-list'>
                 <img src={assets.people_icon}/>
                 <p>Doctor List</p>
             </NavLink>
